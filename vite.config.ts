e627f82c57af323c9e7b8c2b59be0d1657602c84
Julio-Vasquez <jualvalitube@gmail.com>
2025-09-04 14:03:53 -0500

    chore: configure Tailwind CSS with Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import { resolve, join } from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@domain/*': resolve(join(__dirname, 'src/domain/*')),
