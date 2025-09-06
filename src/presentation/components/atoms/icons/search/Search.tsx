49f24c2a4cfc94d5c02ea24dd874f24a03c18322
Julio-Vasquez <jualvalitube@gmail.com>
2025-09-06 12:40:35 -0500

    feat(atom): add Search component

---
+++ src/presentation/components/atoms/icons/search/Search.tsx
@@ -0,0 +1,18 @@
+const Search = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    className='size-5 text-neutral-400'
+    fill='none'
+    viewBox='0 0 24 24'
+    stroke='currentColor'
+  >
+    <path
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      strokeWidth={2}
+      d='M21 21l-4.35-4.35m0 0A7.5 7.5 0 104.5 4.5a7.5 7.5 0 0012.15 12.15z'
+    />
+  </svg>
+)
+
+export default Search
