fe870a81f1932ba2b51fdb46a555b6dbb2224b60
Julio-Vasquez <jualvalitube@gmail.com>
2025-09-06 15:52:49 -0500

    feat(template): add initial Layout with tests

--- src/presentation/components/templates/Layout/Layout.tsx
+++ src/presentation/components/templates/Layout/Layout.tsx
@@ -1,27 +1,32 @@
 import type { LayoutProps } from './layout-type'
+import SearchBar from '@presentation/components/organisms/search-bar'
 
-export default function Layout({ children }: LayoutProps) {
-  return (
-    <div className="min-h-dvh bg-neutral-50 text-neutral-900">
-      <div className="mx-auto min-h-dvh md:grid md:grid-cols-[320px_1fr] xl:max-w-7xl bg-white">
-        {/* Sidebar */}
-        <aside className="flex flex-col border-r border-neutral-200 bg-white md:bg-[#fcfcfc] min-h-dvh">
-          <div className="px-6 py-5 border-b border-neutral-100">
-            <h1 className="text-lg font-semibold">Rick and Morty list</h1>
-            <input placeholder="Buscar" />
+const Layout = ({ children }: LayoutProps) => (
+  <div
+    className='min-h-dvh bg-white text-neutral-900 font-greycliff'
+    data-testid='main-layout'
+  >
+    <div className='min-h-dvh md:grid md:grid-cols-[320px_1fr] bg-white'>
+      {/* Sidebar */}
+      <aside className='flex flex-col border-r border-neutral-200 md:bg-[#fcfcfc] min-h-dvh'>
+        <div className='px-6 py-5 border-b md:bg-[#fcfcfc] border-neutral-100'>
+          <div className='pb-4 px-2'>
+            <h1 className='font-semibold text-2xl'>Rick and Morty list</h1>
           </div>
-          <div className="flex-1 overflow-y-auto">
-            <div className="p-6 text-sm text-neutral-500">Sidebar content</div>
-          </div>
-        </aside>
-
-        {/* Main Content */}
-        <main className="hidden md:block min-h-dvh" aria-label="detail-panel">
-          <div className="h-full overflow-y-auto">
-            <div className="px-8 lg:px-16 py-8">{children}</div>
-          </div>
-        </main>
-      </div>
+          <SearchBar />
+        </div>
+        <div className='flex-1 overflow-y-auto'>
+          <div className='p-6 text-sm text-neutral-500'>Sidebar content</div>
+        </div>
+      </aside>
+      {/* Main Content */}
+      <main className='hidden md:block min-h-dvh' aria-label='detail-panel'>
+        <div className='h-full overflow-y-auto'>
+          <div className='px-8 lg:px-16 py-8'>{children}</div>
+        </div>
+      </main>
     </div>
-  )
-}
+  </div>
+)
+
+export default Layout
--- src/presentation/components/templates/Layout/__tests__/Layout.spec.tsx
+++ src/presentation/components/templates/Layout/__tests__/Layout.spec.tsx
@@ -10,10 +10,13 @@ describe('Layout', () => {
         <p>Detail content</p>
       </Layout>
     )
-
+    const font = screen.getByTestId('main-layout')
     expect(screen.getByText('Rick and Morty list')).toBeInTheDocument()
-    expect(screen.getByPlaceholderText('Buscar')).toBeInTheDocument()
+    expect(
+      screen.getByPlaceholderText('Search or filter results')
+    ).toBeInTheDocument()
     expect(screen.getByText('Detail content')).toBeInTheDocument()
     expect(screen.getByLabelText('detail-panel')).toBeInTheDocument()
+    expect(font).toHaveClass('font-greycliff')
   })
 })
