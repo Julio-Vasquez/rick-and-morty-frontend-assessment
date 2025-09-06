b2ff172e0b736760b7e2aa1a1caa6892e126a186
Julio-Vasquez <jualvalitube@gmail.com>
2025-09-06 12:11:17 -0500

    refactor(test): consolidate Vitest config into Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,30 @@
-import { defineConfig } from 'vite'
-import { resolve, join } from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig, mergeConfig } from 'vite'
+import { defineConfig as defineConfigVitest } from 'vitest/config'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: [
-      { find: '@domain', replacement: resolve(join(__dirname, 'src/domain')) },
-      {
-        find: '@application',
-        replacement: resolve(join(__dirname, 'src/application')),
-      },
-      {
-        find: '@infrastructure',
-        replacement: resolve(join(__dirname, 'src/infrastructure')),
-      },
-      {
-        find: '@presentation',
-        replacement: resolve(join(__dirname, 'src/presentation')),
-      },
-    ],
+const viteConfig = defineConfig({
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
+})
+
+const vitestConfig = defineConfigVitest({
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: './src/setupTests.ts',
+    css: true,
+    coverage: {
+      all: false,
+      provider: 'v8',
+      reporter: ['text', 'html', 'lcov'],
+      reportsDirectory: './coverage',
+      include: ['src/**/*.{ts,tsx}'],
+    },
+    exclude: ['node_modules', 'dist'],
+    include: ['src/**/__tests__/**/*.{test,spec}.{ts,tsx}'],
   },
 })
+
+export default mergeConfig(viteConfig, vitestConfig)
