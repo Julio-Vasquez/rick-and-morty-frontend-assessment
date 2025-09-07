f82fa1d5623d60e9415eef05f5d1bd0c33e34f33
Julio-Vasquez <jualvalitube@gmail.com>
2025-09-07 02:33:08 -0500

    feat: add FilterDrawer component

---
+++ src/presentation/components/organisms/filter-drawer/FilterDrawer.tsx
@@ -0,0 +1,44 @@
+import FilterContent from '../filter-content/FilterContent'
+
+type FilterDrawerProps = { onClose: () => void }
+
+const FilterDrawer = ({ onClose }: FilterDrawerProps) => {
+  return (
+    <>
+      <div
+        className='fixed inset-0 bg-black/40'
+        onClick={onClose}
+        aria-hidden='true'
+      />
+
+      <div
+        className='
+          fixed inset-x-0 bottom-0
+          top-[max(env(safe-area-inset-top),16px)]
+          bg-white rounded-t-3xl shadow-xl
+          flex flex-col
+        '
+        role='dialog'
+        aria-modal='true'
+        aria-label='Filters'
+      >
+        <div className='relative h-12 flex items-center'>
+          <button
+            onClick={onClose}
+            className='absolute left-4 text-purple-600'
+            aria-label='Go back'
+          >
+            &#8592;
+          </button>
+          <h2 className='mx-auto text-lg font-semibold'>Filters</h2>
+        </div>
+
+        <div className='flex-1 overflow-y-auto p-4'>
+          <FilterContent onClose={onClose} />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default FilterDrawer
