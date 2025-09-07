68c516ff36f4af1dae6893a662990ebc0f021c94
Julio-Vasquez <jualvalitube@gmail.com>
2025-09-07 12:09:34 -0500

    feat: add Sort component

---
+++ src/presentation/components/atoms/icons/sort/Sort.tsx
@@ -0,0 +1,58 @@
+const Sort = () => (
+  <svg
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    xmlns='http://www.w3.org/2000/svg'
+    xmlnsXlink='http://www.w3.org/1999/xlink'
+    data-iconid='chevron-sorting-order'
+    data-svgname='Chevron sorting order'
+  >
+    <g>
+      <line
+        x1='12'
+        y1='19.5'
+        x2='7'
+        y2='14.3'
+        fill='none'
+        stroke='#000'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <line
+        x1='12'
+        y1='19.5'
+        x2='17'
+        y2='14.3'
+        fill='none'
+        stroke='#000'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </g>
+    <g>
+      <line
+        x1='12'
+        y1='4.5'
+        x2='7'
+        y2='9.7'
+        fill='none'
+        stroke='#000'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <line
+        x1='12'
+        y1='4.5'
+        x2='17'
+        y2='9.7'
+        fill='none'
+        stroke='#000'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </g>
+  </svg>
+)
+
+export default Sort
