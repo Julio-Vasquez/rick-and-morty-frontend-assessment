346424bf0b23d2651384ed749b14abd79e0d4173
Julio-Vasquez <jualvalitube@gmail.com>
2025-09-07 12:30:43 -0500

    feat: validate if character is a favorite

--- src/presentation/pages/Character.tsx
+++ src/presentation/pages/Character.tsx
@@ -1,16 +1,22 @@
 import { useEffect } from 'react'
 import { useParams } from 'react-router'
 
+import { useFavorites } from '@presentation/hooks/contexts/useFavorites'
 import { useCharacterById } from '@presentation/hooks/api/useCharacterById'
 import CharacterDetailsCard from '@presentation/components/organisms/character-detail-card'
 
 const Character = () => {
   const { id } = useParams()
+  const { isFavorite } = useFavorites()
   const { fetchById, item } = useCharacterById()
 
   useEffect(() => {
     if (id) void fetchById(id)
   }, [id, fetchById])
+
+  if (!id) return null
+
+  const favorite = isFavorite(id)
 
   if (!item) return null
 
@@ -22,6 +28,7 @@ const Character = () => {
       status={item.status}
       species={item.species}
       occupation={item.occupation}
+      isFavorite={favorite}
     />
   )
 }
