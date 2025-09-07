031af4c95503fe1ecae8351a937cdc409b12697d
Julio-Vasquez <jualvalitube@gmail.com>
2025-09-07 12:17:36 -0500

    feat: add button to sort data

--- src/presentation/components/organisms/character-list-section/CharacterListSection.tsx
+++ src/presentation/components/organisms/character-list-section/CharacterListSection.tsx
@@ -1,20 +1,26 @@
 import Title from '@presentation/components/atoms/title/Title'
 import type { CharacterListSectionProps } from './character-list-type'
 import CharacterItem from '@presentation/components/organisms/character-item'
+import Sort from '@presentation/components/atoms/icons/sort'
 
 const CharacterListSection = ({
   title,
   items,
   isFavorite = false,
   onToggleFavorite,
-  heightClass = 'max-h-[33.333dvh]',
+  heightClass,
   emptyText = 'No results',
   className = '',
+  onSort,
 }: CharacterListSectionProps) => {
   return (
     <section className={className}>
-      <Title count={items.length} tag='h3' text={title} />
-
+      <div className='flex items-center justify-between'>
+        <Title count={items.length} tag='h3' text={title} />
+        <button className='cursor-pointer' onClick={onSort}>
+          <Sort />
+        </button>
+      </div>
       <div className={`${heightClass} overflow-y-auto scrollbar-none`}>
         {items.length === 0 ? (
           <p className='px-4 py-8 text-sm text-neutral-400'>{emptyText}</p>
--- src/presentation/components/organisms/character-list-section/character-list-type.ts
+++ src/presentation/components/organisms/character-list-section/character-list-type.ts
@@ -8,4 +8,5 @@ export interface CharacterListSectionProps {
   heightClass?: string // ej: "max-h-[33.333dvh]" o "flex-1 min-h-0"
   emptyText?: string
   className?: string
+  onSort: () => void
 }
