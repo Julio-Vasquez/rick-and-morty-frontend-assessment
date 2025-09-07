c097b6e7e90ca9dca690454ddab558c34c1e4080
Julio-Vasquez <jualvalitube@gmail.com>
2025-09-07 12:11:11 -0500

    feat: search on press enter

--- src/presentation/components/molecules/input/Input.tsx
+++ src/presentation/components/molecules/input/Input.tsx
@@ -1,18 +1,32 @@
-import { useState } from 'react'
+import { useRef, useState, type KeyboardEvent } from 'react'
 
 import FilterDrawer from '@presentation/components/organisms/filter-drawer'
 import FilterPopup from '@presentation/components/organisms/filter-popup'
 
 import type { InputFieldProps } from './input-type'
 
-const InputField = ({ leftIcon, rightIcon, ...props }: InputFieldProps) => {
+const InputField = ({ leftIcon, rightIcon, refetch, ...props }: InputFieldProps) => {
   const [open, setOpen] = useState(false)
+  const ref = useRef<HTMLInputElement>(null)
+
+  const handleSearch = () => {
+    console.log(ref.current?.value)
+    if (ref.current?.value) refetch({ name: ref.current?.value })
+  }
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleSearch()
+    }
+  }
 
   return (
     <div className='flex items-center w-full bg-[#f3f4f6] rounded-xl px-3 py-1 shadow-sm'>
       {leftIcon && <span className='mr-2'>{leftIcon}</span>}
       <input
+        ref={ref}
         {...props}
+        onKeyDown={handleKeyDown}
         className='flex-1 bg-transparent outline-none text-sm text-neutral-700 placeholder:text-neutral-400 placeholder:text-[16px]'
       />
       {rightIcon && (
@@ -26,10 +40,10 @@ const InputField = ({ leftIcon, rightIcon, ...props }: InputFieldProps) => {
       {open && (
         <>
           <div className='block md:hidden'>
-            <FilterDrawer onClose={() => setOpen(false)} />
+            <FilterDrawer onClose={() => setOpen(false)} refetch={refetch} />
           </div>
           <div className='hidden md:block absolute mt-2 top-28 left-5 z-10'>
-            <FilterPopup onClose={() => setOpen(false)} />
+            <FilterPopup onClose={() => setOpen(false)} refetch={refetch} />
           </div>
         </>
       )}
--- src/presentation/components/molecules/input/input-type.ts
+++ src/presentation/components/molecules/input/input-type.ts
@@ -1,8 +1,10 @@
 import type { InputHTMLAttributes } from 'react'
-
+import type { CharacterListParams } from '@domain/types/character-list-params'
 export interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   /** Optional left icon */
   leftIcon: React.ReactNode
   /** Optional right icon */
   rightIcon: React.ReactNode
+
+  refetch: (params?: CharacterListParams) => Promise<void>
 }
