548bc7e9f28edfdf9255b43fb2c2d87abc2674fa
Julio-Vasquez <jualvalitube@gmail.com>
2025-09-07 12:05:32 -0500

    feat: arrow left button component

---
+++ src/presentation/components/atoms/icons/arrow-left/ArrowLeft.tsx
@@ -0,0 +1,16 @@
+const ArrowLeft = () => {
+  return (
+    <svg width='20' height='20' viewBox='0 0 24 24' aria-hidden='true'>
+      <path
+        d='M15 6l-6 6 6 6'
+        fill='none'
+        stroke='currentColor'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
+
+export default ArrowLeft
